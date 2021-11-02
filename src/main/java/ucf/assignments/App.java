42f96924a3929c47eec2cd9493f240ec904ea90e
michaelvuolo <michaelvuolo1@gmail.com>
2021-11-01 23:50:06 -0400

    done

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -5,6 +5,34 @@
 
 package ucf.assignments;
 
-public class App {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    // launch the application
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    // load FXML file and display it
+    @Override
+    public void start(Stage stage) {
+        try {
+            // load fxml file
+            Parent root = FXMLLoader.load(getClass().getResource("/app.fxml"));
+
+            // display fxml
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("To Do List");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
